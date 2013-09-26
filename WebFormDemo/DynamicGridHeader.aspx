a32eda63dfcf170806c061aa9636bbaf951e9a0f
nbnbnb <nbnbnb.zhang@gmail.com>
2013-09-26 22:43:43 +0800

    添加 动态表头 Demo

---
+++ WebFormDemo/DynamicGridHeader.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="DynamicGridHeader.aspx.cs" Inherits="WebFormDemo_DynamicGridHeader" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>动态表头</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:GridView runat="server" ID="GridView_Main" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebFormDemo/DynamicGridHeader.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class WebFormDemo_DynamicGridHeader : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        BindEvent();
+        BindSource();
+    }
+
+    private void BindEvent()
+    {
+        GridView_Main.RowDataBound += GridView_Main_RowDataBound;
+    }
+
+    void GridView_Main_RowDataBound(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.Header)
+        {
+            for (int i = 0; i < e.Row.Cells.Count; i++)
+            {
+                e.Row.Cells[i].Text = i.ToString();
+                e.Row.Cells[i].Width = (1 + i) * 100;
+            }
+        }
+
+        if (e.Row.RowIndex % 3 == 0)
+        {
+            e.Row.BackColor = System.Drawing.Color.Red;
+        }
+    }
+
+    private void BindSource()
+    {
+        GridView_Main.DataSource = GetSource();
+        GridView_Main.DataBind();
+    }
+
+    private List<Person> GetSource()
+    {
+        List<Person> persons = new List<Person>();
+        persons.Add(new Person
+        {
+            Name = "ZhangJin",
+            Address = "WuHan",
+            Age = 26
+        });
+
+        persons.Add(new Person
+        {
+            Name = "张进",
+            Address = "武汉",
+            Age = 26
+        });
+        persons.Add(new Person
+        {
+            Name = "ZhangJin",
+            Address = "WuHan",
+            Age = 26
+        });
+
+        persons.Add(new Person
+        {
+            Name = "张进",
+            Address = "武汉",
+            Age = 26
+        });
+        persons.Add(new Person
+        {
+            Name = "ZhangJin",
+            Address = "WuHan",
+            Age = 26
+        });
+
+        persons.Add(new Person
+        {
+            Name = "张进",
+            Address = "武汉",
+            Age = 26
+        });
+
+        return persons;
+    }
+
+    private class Person
+    {
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public int Age { get; set; }
+    }
+}
