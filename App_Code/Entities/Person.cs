c58ea4703454e1712f018f264cea989d2c8ea72e
ZhangJin <nbnbnb.zhang@gmail.com>
2013-11-07 01:12:16 +0800

    Sync to Server

---
+++ App_Code/Entities/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoSite.Entities
+{
+
+    public class Person
+    {
+        public string UserName { get; set; }
+
+        public string UserAddress { get; set; }
+
+        public int Age { get; set; }
+    }
+}
