877a9fbd617a6167f8887d522d694fa69346ce4d
ZhangJin <nbnbnb.zhang@gmail.com>
2013-11-16 02:01:44 +0800

    Add USBDog Project

---
+++ App/DogInit/MainForm.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace DogInit
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Init = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Init
+            // 
+            this.btn_Init.Font = new System.Drawing.Font("宋体", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_Init.Location = new System.Drawing.Point(107, 65);
+            this.btn_Init.Name = "btn_Init";
+            this.btn_Init.Size = new System.Drawing.Size(172, 78);
+            this.btn_Init.TabIndex = 0;
+            this.btn_Init.Text = "初始化";
+            this.btn_Init.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 240);
+            this.Controls.Add(this.btn_Init);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "加密狗初始化";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Init;
+    }
+}
+
---
+++ USBDog/ClientCheck.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ClientCheck.aspx.cs" Inherits="DemoSite.USBDog.ClientCheck" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>客户端检测</title>
+    <script src="../Scripts/jquery-1.9.1.js"></script>
+</head>
+<body>
+    <embed id="s_simnew31"  type="application/npsyunew3-plugin" hidden="true" />
+    <form id="form1" runat="server">
+        <div>
+            <fieldset>
+                <legend>说明</legend>
+                <p>通过在客户端生成随机数，然后通过硬件加密后，服务器端也通过随机数进行加密，然后比较是否相同</p>
+                <p>
+                    <input type="button" value="检测" id="client_check" />
+                </p>
+            </fieldset>
+        </div>
+    </form>
+
+    <script type="text/javascript">
+        $(function () {
+
+            $("#client_check").bind("click", function () {
+
+                var s_simnew31;
+                //创建插件或控件
+                if (navigator.userAgent.indexOf("MSIE") > 0 && !navigator.userAgent.indexOf("opera") > -1) {
+                    s_simnew31 = new ActiveXObject("Syunew3A.s_simnew3");
+                }
+                else {
+                    s_simnew31 = document.getElementById("s_simnew31"); 
+                }
+
+                //查找是否存在锁,这里使用了FindPort函数
+                var devicePath = s_simnew31.FindPort(0);
+                if (s_simnew31.LastError != 0) {
+                    window.alert("未发现加密锁，请插入加密锁");
+                    return;
+                }
+
+                // 注意，此处一定需要转换为字符串
+                var key = new Date().getTime().toString();
+
+                var value = s_simnew31.EncString(key, devicePath);
+   
+                $.get("check.dog",
+                    { key: key, value: value },
+                    function (data) {
+                        if (data === "true") {
+                            alert("检测通过!");
+                        } else {
+                            alert("检测失败!");
+                        }
+                    });
+            });
+
+        });
+
+
+    </script>
+
+</body>
+</html>
