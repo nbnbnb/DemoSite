ee8f7aadcb21a1c6bc1e4c97945084d6602953f3
NBZhang <nbnbnb.zhang@gmail.com>
2014-10-25 00:01:25 +0800

    优化 BaiduMap

---
+++ Site/BaiduMap/MapTextView.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="MapTextView.aspx.cs" Inherits="BaiduMap_MapTextView" %>
+
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1,maximum-scale=1">
+
+    <link href="../Styles/bmapview.css" rel="stylesheet" />
+    <script type="text/javascript" src="http://api.map.baidu.com/api?v=2.0&ak=mNKo9VLn4a7oONWaOSTT9j3w"></script>
+    <script src="../Scripts/jquery-2.1.1.js"></script>
+    <script src="../Scripts/underscore.js"></script>
+    <script src="../Scripts/convertor.js"></script>
+    <script src="../Scripts/knockout-3.2.0.js"></script>
+    <script src="../Scripts/BMapPage.js"></script>
+
+    <title>文字详情</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <div class="textview">
+            <div class="main-content">
+                <div class="stat">
+                    <p><span data-bind="text:lineType"></span>至<span data-bind="text:hotelName"></span>的路线</p>
+                    <p><span data-bind="text:minute"></span>&nbsp;&nbsp;<span data-bind="text:mile"></span><span data-bind="click:goPicView" class="viewMap">查看地图</span></p>
+                </div>
+
+                <div class="route">
+                    <p class="start">
+                        <span>&nbsp;</span>起点（<b>我的位置</b>）
+                    </p>
+                    <div data-bind="foreach:details" class="details">
+                        <p data-bind="html:$data"></p>
+                    </div>
+                    <p class="end">
+                        终点（<b data-bind="text:hotelName"></b>）
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div id="allmap"></div>
+    </form>
+
+    <script>
+        $(function () {
+            var begin_lng=<%= Request.QueryString["begin_lng"] %>;
+            var begin_lat=<%= Request.QueryString["begin_lat"] %>;
+            var end_lng=<%= Request.QueryString["end_lng"] %>;
+            var end_lat=<%= Request.QueryString["end_lat"] %>;
+            var lineType="<%= Request.QueryString["lineType"] %>";
+            var hotelName="<%= Request.QueryString["hotelName"] %>";
+            var minute="<%= Request.QueryString["minute"] %>";
+            var mile="<%= Request.QueryString["mile"] %>";
+
+            BMapPage.TextViewInit(begin_lng,begin_lat,end_lng,end_lat,hotelName,minute,mile,lineType,'allmap');
+        });
+    </script>
+
+</body>
+</html>
--- Site/Scripts/BMapPage.js
+++ Site/Scripts/BMapPage.js
@@ -1,11 +1,11 @@
 ﻿window.BMapPage = window.BMapPage || {};
 window.BMapHelper = window.BMapHelper || {};
 
-window.BMapHelper.createMap = function (id) {
+BMapHelper.createMap = function (id) {
     return new BMap.Map(id);
 };
 
-window.BMapHelper.addZoomInOutControl = function (left, right, map) {
+BMapHelper.addZoomInOutControl = function (left, right, map) {
     //左上角，缩放按钮
     var top_right_navigation =
         new BMap.NavigationControl({ anchor: BMAP_ANCHOR_TOP_RIGHT, type: BMAP_NAVIGATION_CONTROL_SMALL });
@@ -13,24 +13,14 @@ window.BMapHelper.addZoomInOutControl = function (left, right, map) {
     map.addControl(top_right_navigation);
 };
 
-window.BMapHelper.createPoint = function (x, y) {
+BMapHelper.createPoint = function (x, y) {
     return new BMap.Point(x, y);
 };
 
-window.BMapHelper.doSearch = function (start_point, end_point, lineType, map, options) {
-    BMap.Convertor.translate(start_point, 0, function (start) {
-        BMap.Convertor.translate(end_point, 2, function (end) {
-            map.clearOverlays();
-            new lineType(map, options).search({ title: '你当前的位置', point: start },
-                { title: '目的地', point: end });
-        });
-    });
-};
-
-window.BMapHelper.doMark = function (point, coorType, content, map) {
+BMapHelper.doMark = function (point, coorType, content, map) {
     BMap.Convertor.translate(point, coorType, function (res_point) {
         var count = 0,
-            label = new BMap.Label(content, { offset: new BMap.Size(20, -10) }),
+            label = new BMap.Label(content, { offset: new BMap.Size(20, -10) }), // 标签位置
             marker = new BMap.Marker(res_point, { title: '测试标点' }),
             // 设置跳动动画
             timerId = setInterval(function () {
@@ -50,32 +40,37 @@ window.BMapHelper.doMark = function (point, coorType, content, map) {
     });
 };
 
-BMapPage._initCoor = function (begin_lng, begin_lat, end_lng, end_lat) {
-    this._begin_lng = begin_lng;
-    this._begin_lat = begin_lat;
-    this._end_lng = end_lng;
-    this._end_lat = end_lat;
+BMapHelper.createOption = function (map, callback) {
+    return {
+        renderOptions: {
+            map: map,
+            autoViewport: true
+        },
+        onSearchComplete: callback
+    };
 };
 
-BMapPage._initMap = function () {
-    this._map = window.BMapHelper.createMap("allmap");
-    window.BMapHelper.addZoomInOutControl(30, 10, this._map);
-
-    // 如果能进入这个页面表示可以获得设备的经纬度
-    this._start_point = window.BMapHelper.createPoint(this._begin_lng, this._begin_lat); // 硬件坐标【0】
-    this._end_point = window.BMapHelper.createPoint(this._end_lng, this._end_lat);  // Google 地图坐标【2】
-
-    this._map.enableScrollWheelZoom(true);
-    this._map.centerAndZoom(this._start_point, 15);
+BMapHelper.lineSearch = function (config, map, start_point, end_point) {
+    new config.type(map, BMapHelper.createOption(map, config.callback)).search(start_point, end_point);
 };
 
-BMapPage.TextViewInit = function (begin_lng, begin_lat, end_lng, end_lat, hotelName, minute,
-    mile, lineType, isWarp, memberId) {
+BMapPage.ViewInit = function (view, eleId, begin_lng, begin_lat, end_lng, end_lat) {
+    view.map = BMapHelper.createMap(eleId);
+    view.start_point = BMapHelper.createPoint(begin_lng, begin_lat); // 硬件坐标【0】
+    view.end_point = BMapHelper.createPoint(end_lng, end_lat);  // Google 地图坐标【2】
+    view.map.centerAndZoom(view.start_point, 15);  // 必须要添加此行，才不会导致脚本错误
 
-    this._initCoor(begin_lng, begin_lat, end_lng, end_lat);
-    this._initMap();
+    view.map.enableScrollWheelZoom(true);
+    view.map.centerAndZoom(view.start_point, 15);  // 必须要调用此方法，该不会导致  Uncaught TypeError: Cannot read property 'Or' of undefined  错误
+    BMapHelper.addZoomInOutControl(30, 10, view.map);
+};
 
-    var self = this;
+BMapPage.TextViewInit = function (begin_lng, begin_lat, end_lng, end_lat, hotelName, minute, mile, lineType, eleId) {
+    if (!this instanceof BMapPage.TextViewInit) {
+        return new BMapPage.TextViewInit(begin_lng, begin_lat, end_lng, end_lat, hotelName, minute, mile, lineType, eleId);
+    }
+
+    BMapPage.ViewInit(this, eleId, begin_lng, begin_lat, end_lng, end_lat);
 
     var walkAndDriveSearchComplete = function (result) {
         var route = result.getPlan(0).getRoute(0);  // 返回第一个方案的第一条线路
@@ -83,177 +78,101 @@ BMapPage.TextViewInit = function (begin_lng, begin_lat, end_lng, end_lat, hotelN
         for (var i = 0, len = route.getNumSteps() ; i < len ; i++) {
             model.details.push("<b>" + (i + 1) + "</b>. " + route.getStep(i).getDescription(false));
         }
-    };
+    },
+      busSearchComplete = function (result) {
+          model.details.removeAll();
+          for (var i = 0, len = result.getNumPlans() ; i < len; i++) {
+              model.details.push("<b>" + (i + 1) + "</b>. " + result.getPlan(i).getDescription(false));
+          }
+      },
+      searchMapping = {
+          "步行": { type: BMap.WalkingRoute, callback: walkAndDriveSearchComplete },
+          "公交": { type: BMap.TransitRoute, callback: busSearchComplete },
+          "驾车": { type: BMap.DrivingRoute, callback: walkAndDriveSearchComplete }
+      },
+      MyModel = function () {
+          this.lineType = lineType;
+          this.hotelName = hotelName;
+          this.minute = minute;
+          this.mile = mile;
+          this.details = ko.observableArray(['加载数据中...']);
+          this.goPicView = function () {
+              history.back();
+          };
+      },
+      model = new MyModel();
 
-    var busSearchComplete = function (result) {
-        model.details.removeAll();
-        for (var i = 0, len = result.getNumPlans() ; i < len; i++) {
-            model.details.push("<b>" + (i + 1) + "</b>. " + result.getPlan(i).getDescription(false));
-        }
-    };
+    ko.applyBindings(model);
 
-    var walk_drive_options = {
-        renderOptions: {
-            map: self._map,
-            autoViewport: true
-        },
-        onSearchComplete: walkAndDriveSearchComplete
-    };
+    BMapHelper.lineSearch(searchMapping[lineType], this.map, this.start_point, this.end_point);
+};
 
-    var bus_options = {
-        renderOptions: {
-            map: self._map,
-            autoViewport: true
-        },
-        onSearchComplete: busSearchComplete
-    };
+BMapPage.PicViewInit = function (begin_lng, begin_lat, end_lng, end_lat, hotelName, eleId) {
+    if (!this instanceof BMapPage.PicViewInit) {
+        return new BMapPage.PicViewInit(begin_lng, begin_lat, end_lng, end_lat, hotelName);
+    }
 
-    var searchWalkLine = function () {
-        new BMap.WalkingRoute(self._map, walk_drive_options).search(self._start_point, self._end_point);
-    };
+    BMapPage.ViewInit(this, eleId, begin_lng, begin_lat, end_lng, end_lat);
 
-    var searchBusLine = function () {
-        new BMap.TransitRoute(self._map, bus_options).search(self._start_point, self._end_point);
-    };
-
-    var searchDriveLine = function () {
-        new BMap.DrivingRoute(self._map, walk_drive_options).search(self._start_point, self._end_point);
-    };
-
-    var searchMapping = {
-        "步行": searchWalkLine,
-        "公交": searchBusLine,
-        "驾车": searchDriveLine
-    };
-
-    searchMapping[lineType]();
-
-    var MyModel = function () {
-
-        this.isWarp = isWarp;
-        this.lineType = lineType;
-        this.hotelName = hotelName;
-        this.minute = minute;
-        this.mile = mile;
-        this.details = ko.observableArray(['加载数据中...']);
-        this.goPicView = function () {
-            //history.back();
-            var params = {
-                begin_lng: begin_lng,
-                begin_lat: begin_lat,
-                end_lng: end_lng,
-                end_lat: end_lat,
-                hotelName: hotelName,
-                lineType: lineType,
-                memberId: memberId
-            };
-            window.location = "MapPicView.aspx?" + $.param(params);
+    var isAssist = false, // 当没有合适路线时，自动分配驾车路线
+        self = this;
+    searchComplete = function (results) {
+        if (!isAssist && results.getNumPlans() === 0) {
+            isAssist = true;
+            model.selectedIndex(3);
+            doSearch(BMap.DrivingRoute);
+            alert('无法找到合适步行线路，为您选择驾车方式');
+            return;
         };
-    };
-
-    var model = new MyModel();
+        var plan = results.getPlan(0);
+        model.mile(plan.getDistance(true));
+        model.minute(plan.getDuration(true));
+        model.showStat(true);
+    },
+ doSearch = function (searchType) {
+     BMapHelper.lineSearch({ type: searchType, callback: searchComplete }, self.map, self.start_point, self.end_point);
+ },
+ doMarker = function (markPoint, coorType, content) {
+     BMapHelper.doMark(markPoint, coorType, content, self.map);
+ },
+ MyViewModel = function () {
+     this.selectedIndex = ko.observable();
+     this.showStat = ko.observable(false);
+     this.minute = ko.observable();
+     this.mile = ko.observable();
+     this.walk = function () {
+         this.selectedIndex(1);
+         doSearch(BMap.WalkingRoute);
+     };
+     this.bus = function () {
+         this.selectedIndex(2);
+         doSearch(BMap.TransitRoute);
+     };
+     this.drive = function () {
+         this.selectedIndex(3);
+         doSearch(BMap.DrivingRoute);
+     };
+     this.locateStart = function () {
+         doMarker(self.start_point, 0, '当前位置');
+     };
+     this.locateEnd = function () {
+         doMarker(self.end_point, 2, hotelName);
+     };
+     this.goTextView = function () {
+         var params = {
+             minute: this.minute(),
+             mile: this.mile(),
+             begin_lng: begin_lng,
+             begin_lat: begin_lat,
+             end_lng: end_lng,
+             end_lat: end_lat,
+             hotelName: hotelName
+         },
+         lineType = this.selectedIndex() === 1 ? "步行" : this.selectedIndex() === 2 ? "公交" : "驾车";
+         window.location = "MapTextView.aspx?" + $.param(params) + "&lineType=" + lineType;
+     };
+ },
+ model = new MyViewModel();
+    // Model Bind
     ko.applyBindings(model);
 };
-
-BMapPage.PicViewInit = function (begin_lng, begin_lat, end_lng, end_lat, hotelName, isWarp, memberId) {
-
-    this._initCoor(begin_lng, begin_lat, end_lng, end_lat);
-    this._initMap();
-
-    var self = this,
-        isAssist = false, // 当没有合适路线时，自动分配驾车路线
-        searchComplete = function (results) {
-            if (!isAssist && results.getNumPlans() === 0) {
-                model.showStat(false);
-                isAssist = true;
-                $('span[class=drive]').click();
-                alert('无法找到合适步行线路，为您选择驾车方式');
-                return;
-            };
-
-            isAssist = false;
-            var plan = results.getPlan(0);
-            model.mile(plan.getDistance(true));
-            model.minute(plan.getDuration(true));
-            model.showStat(true);
-        },
-        options = {
-            renderOptions: {
-                map: self._map,
-                autoViewport: true
-            },
-            onSearchComplete: searchComplete
-        },
-
-        doSearch = function (lineType) {
-            window.BMapHelper.doSearch(self._start_point, self._end_point, lineType, self._map, options);
-        },
-        doMarker = function (point, coorType, content) {
-            window.BMapHelper.doMark(point, coorType, confirm, self._map);
-        },
-        MyViewModel = function () {
-            this.selectedIndex = ko.observable();
-            this.showStat = ko.observable(false);
-            this.minute = ko.observable();
-            this.mile = ko.observable();
-            this.isWarp = isWarp;
-            this.walk = function () {
-                this.selectedIndex(1);
-                doSearch(BMap.WalkingRoute);
-            };
-            this.bus = function () {
-                this.selectedIndex(2);
-                doSearch(BMap.TransitRoute);
-            };
-            this.drive = function () {
-                this.selectedIndex(3);
-                doSearch(BMap.DrivingRoute);
-            };
-            this.locateStart = function () {
-                doMarker(self._start_point, 0, '当前位置');
-            };
-            this.locateEnd = function () {
-                doMarker(self._end_point, 2, hotelName);
-            };
-            this.goTextView = function () {
-                var params = {
-                    minute: this.minute(),
-                    mile: this.mile(),
-                    begin_lng: begin_lng,
-                    begin_lat: begin_lat,
-                    end_lng: end_lng,
-                    end_lat: end_lat,
-                    hotelName: hotelName,
-                    memberId: memberId
-                },
-                lineType = this.selectedIndex() === 1 ? "步行" : this.selectedIndex() === 2 ? "公交" : "驾车";
-                window.location = "MapTextView.aspx?" + $.param(params) + "&lineType=" + lineType;
-            };
-        },
-
-        model = new MyViewModel();
-
-    // Model Bind
-    ko.applyBindings(new MyViewModel());
-};
-
-$(function () {
-    var url = decodeURI(location.href.toString()),  // 中文需要解码   
-        types = ['驾车', '公交', '步行'],
-        selectors = ['drive', 'bus border', 'walk border'],
-        trigger = function (index) {
-            setTimeout(function () {
-                $('span[class="' + selectors[index] + '"]').click();
-            }, 100);
-        };
-
-    if (url.indexOf('lineTpye') < 0) {// 默认加载时选择步行
-        trigger(2);
-    } else {
-        _.forEach(types, function (ele, index, list) {
-            if (url.indexOf('lineType=' + ele) > 0) {
-                trigger(index);
-            }
-        });
-    }
-});
