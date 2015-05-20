b4fc03dc766967e065577fe987097ca59fec9055
ZhangJin <nbnbnb.zhang@gmail.com>
2015-05-20 21:52:06 +0800

    Sync In Home

---
+++ Site/JSPattern/Version03/Interface.js
@@ -0,0 +1,52 @@
+﻿// construcotr.
+
+var Interface = function (name, methods) {
+    if (arguments.length !== 2) {
+        throw new Error('Interface constructor call with ' +
+            arguments.length + 'arguments, but exceptd exactly 2.');
+    }
+
+    this.name = name;
+    this.methods = [];
+
+    for (var i = 0; i < methods.length; i++) {
+        if (typeof (methods[i]) !== 'string') {
+            throw new Error('Interface constructor expects method names to be passed in a string.');
+        }
+        this.methods.push(methods[i]);
+    }
+};
+
+// static method
+
+Interface.ensureImplements = function (object) {
+    var i,
+        j,
+        len,
+        methodsLen,
+        method,
+        interface;
+
+    if (arguments.length < 2) {
+        throw new Error('Function Interface.ensureImplements called with ' + arguments.length +
+            ' arguments, but expected at least 2.');
+    }
+
+    // 第2个参数后为接口
+    for (i = 1, len = arguments.length; i < len; i++) {
+        interface = arguments[i];
+
+        if (interface.constructor !== Interface) {
+            throw new Error('Function Interface.ensureImplements expects arguments two and above to be instances of Interface.');
+        }
+
+        for (j = 0, methodsLen = interface.methods.length; j < methodsLen; j++) {
+            method = interface.methods[j];
+            if (!object[method] || typeof (object[method]) !== 'function') {
+                throw new Error('Function Interface.ensureImplements: object does not implement the ' +
+                    interface.name + ' interface. Method ' +
+                    method + ' was not found.');
+            }
+        }
+    }
+};
---
+++ Site/JSPattern/Version03/test/Interface.test.js
@@ -0,0 +1,57 @@
+﻿/// <reference path="//code.jquery.com/qunit/qunit-1.17.1.js" />
+/// <reference path="../Interface.js" />
+
+module('Interface 接口测试')
+
+test('检测对象实现了接口', function () {
+    // Arrange
+    var personInterface = new Interface('Person', ['sayName', 'showAge', 'run']);
+    var Person = function (name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Person.prototype.sayName = function () {
+        console.log(this.name);
+    };
+    Person.prototype.showAge = function () {
+        console.log(this.age);
+    };
+    Person.prototype.run = function () {
+        console.log('Running....');
+    };
+
+    // Act
+    var zhangjin = new Person('张进', 28);
+    Interface.ensureImplements(zhangjin, personInterface);
+
+    // Assert
+    ok(true, '对象实现了指定的接口方法');
+});
+
+test('检测对象未实现接口', function () {
+    // Arrange
+    var personInterface = new Interface('Person', ['sayName', 'showAge', 'run', 'swimming']);
+    var Person = function (name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    Person.prototype.sayName = function () {
+        console.log(this.name);
+    };
+    Person.prototype.showAge = function () {
+        console.log(this.age);
+    };
+    Person.prototype.run = function () {
+        console.log('Running....');
+    };
+
+    // Act
+    var zhangjin = new Person('张进', 28);
+
+
+    // Assert
+    throws(function () { Interface.ensureImplements(zhangjin, personInterface); },
+        Error,
+        '对象未实现了指定的接口方法');
+
+});
