b335361f1bd097d7991b970e5a62d20af0096b2b
ZhangJin <jinzhanga@Ctrip.com>
2016-01-21 17:02:19 +0800

    Custom ETag

---
+++ Site/WebFormDemo/CacheDemo/CustomETag.aspx
@@ -0,0 +1,18 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="CustomETag.aspx.cs" Inherits="WebFormDemo_CacheDemo_CustomETag" %>
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>设置 ETag</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <h4>一定要设置为 HttpCacheability.ServerAndPrivate，添加 ETag 才有效</h4>
+
+        <%= DateTime.Now %>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Site/WebFormDemo/CacheDemo/CustomETag.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class WebFormDemo_CacheDemo_CustomETag : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        // 一定要设置为 HttpCacheability.ServerAndPrivate，添加 ETag 才有效
+        Response.Cache.SetCacheability(HttpCacheability.ServerAndPrivate);
+        Response.Cache.SetETag(DateTime.Now.Ticks.ToString());
+
+        //Response.Clear();
+        //Response.StatusCode = 304;
+        //Response.SuppressContent = true;
+        //Response.Flush();
+
+    }
+}
